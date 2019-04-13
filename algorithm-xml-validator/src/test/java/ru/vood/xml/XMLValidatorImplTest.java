e0dedb40040d50e48607aa99c427dd9ba7338ade
balitsky_igor <balitsky_igor@list.ru>
2019-04-13 19:25:12 +0300

    XmlValidator test

--- algorithm-xml-validator/src/test/java/ru/vood/xml/XMLValidatorImplTest.java
+++ algorithm-xml-validator/src/test/java/ru/vood/xml/XMLValidatorImplTest.java
@@ -1,12 +1,26 @@
 package ru.vood.xml;
 
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class XMLValidatorImplTest {
 
+    private XMLValidatorImpl xmlValidator;
+
+    @Before
+    public void before() {
+        xmlValidator = new XMLValidatorImpl();
+    }
+
     @Test
     public void validate() {
-        //Todo
+        xmlValidator.validate("test.xml", "test.xsd");
     }
+
+    @Test(expected = XmlValidationException.class)
+    public void validateError() {
+        xmlValidator.validate("test.error.xml", "test.xsd");
+    }
+
 }
