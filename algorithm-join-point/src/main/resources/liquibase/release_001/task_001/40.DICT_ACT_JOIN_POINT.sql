c1db308ea4a9cae8bbe26b67e3918ea6b774d211
vood <balitsky_igor@list.ru>
2019-05-25 19:01:52 +0300

    join point

--- algorithm-join-point/src/main/resources/liquibase/release_001/task_001/40.DICT_ACT_JOIN_POINT.sql
+++ algorithm-join-point/src/main/resources/liquibase/release_001/task_001/40.DICT_ACT_JOIN_POINT.sql
@@ -1,29 +1,31 @@
 create table dict_act_join_point (
-  id                     varchar2(20)  not null,
-  constraint dict_act_join_point_pk primary key (id)
+                                     id             varchar2(20)  not null,
+                                     constraint dict_act_join_point_pk primary key (id)
     using index tablespace jp_idx,
 --   parent                 varchar2(20),
 --   constraint dict_act_join_point_parent_fk foreign key (parent) references dict_act_join_point (id),
   --
-  description            varchar2(255) not null,
+                                     flow_type      varchar2(20)  not null,
+                                     constraint dict_act_join_flow_type_fk foreign key (flow_type) references dict_act_flow_type (id),
+                                     description    varchar2(255) not null,
   --   listen_event_id  varchar2(20) not null,
   --   constraint dict_act_join_point_gen_evt_fk foreign key (listen_event_id) references dict_act_event (id),
   --   publish_event_id     varchar2(20) not null,
   --   constraint dict_act_join_point_pub_evt_fk foreign key (publish_event_id) references dict_act_event (id),
-  global_timeout         number        not null,
-  constraint dict_act_join_point_timeout_ck check (global_timeout > 0),
+                                     global_timeout number        not null,
+                                     constraint dict_act_join_point_timeout_ck check (global_timeout > 0),
   --
 --   reprocessing_timeout   number,
 --   constraint dict_act_join_point_reptime_ck check (reprocessing_timeout > 0 ),
   --
-  process_bean_name      varchar2(255) not null,
-  constraint dict_act_join_process_bean_fk foreign key (process_bean_name) references dict_act_bean (bean_id),
+                                     process_bean_name      varchar2(255) not null,
+                                     constraint dict_act_join_process_bean_fk foreign key (process_bean_name) references dict_act_bean (bean_id),
   --
 --   reprocessing_bean_name varchar2(255),
 --   constraint dict_act_join_reproces_bean_fk foreign key (reprocessing_bean_name) references dict_act_bean (bean_id),
   --
-  status                 varchar2(1)   not null,
-  constraint dict_act_join_point_state_ck check (status in ('d', 'o', 'm'))
+                                     status                 varchar2(1)   not null,
+                                     constraint dict_act_join_point_state_ck check (status in ('d', 'o', 'm'))
   --
 
 )
--- algorithm-join-point/src/main/resources/liquibase/release_001/task_001/data/500.dict_act_join_point_data.sql
+++ algorithm-join-point/src/main/resources/liquibase/release_001/task_001/data/500.dict_act_join_point_data.sql
@@ -1,8 +1,8 @@
-insert into jp.dict_act_join_point(ID, DESCRIPTION, GLOBAL_TIMEOUT, PROCESS_BEAN_NAME, STATUS)
+insert into jp.dict_act_join_point(ID, DESCRIPTION, GLOBAL_TIMEOUT, PROCESS_BEAN_NAME, STATUS, FLOW_TYPE)
 with d as (select level num
            from DUAL
            connect by level <= 5
 )
-select 'join point ' || d.num, 'join point DESCRIPTION ' || d.num, 1000 * d.num, 'Bean' || d.num, 'm'
+select 'join point ' || d.num, 'join point DESCRIPTION ' || d.num, 1000 * d.num, 'Bean' || d.num, 'm', 'FLOW 1'
 from DUAL
          cross join d
