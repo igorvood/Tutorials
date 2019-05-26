bd1692bbeb3685b5749b737788859088d277acd2
vood <balitsky_igor@list.ru>
2019-05-26 14:41:11 +0300

    join point

--- algorithm-join-point/src/main/resources/liquibase/release_001/task_001/500.ACT_JOIN_POINT.sql
+++ algorithm-join-point/src/main/resources/liquibase/release_001/task_001/500.ACT_JOIN_POINT.sql
@@ -1,24 +1,29 @@
 create table act_join_point
 (
-    id           number       not null,
-    join_point   varchar2(20) not null,
+    id                 number       not null,
+    join_point         varchar2(20) not null,
 
     constraint act_join_point_pk primary key (id, join_point)
         using index tablespace jp_idx,
+    parent_id          number,
+    parent_join_point  varchar2(20),
+    constraint act_join_point_parent_fk foreign key (parent_id, parent_join_point) references act_join_point (id, join_point),
     ---
-    date_beg     timestamp    not null,
+    expire_at          timestamp    not null,
+    timout_detected_at timestamp,
+    date_beg           timestamp,
     ---
     --   request_id         varchar2(20) not null,
     --   responce_id        varchar2(20),
-    date_end     timestamp,
+    date_end           timestamp,
     --
     --
-    state        varchar2(20) not null,
+    state              varchar2(20) not null,
     constraint act_join_point_state_fk foreign key (state) references dict_act_state (id),
     ---
     is_closed as (
         case
-            when act_join_point.date_end is null
+            when date_end is null or timout_detected_at is null
                 then 0
             else 1 end ),
     ---
