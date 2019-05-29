6b09809f8d31143ee50d957b4bc808e287c216d6
vood <balitsky_igor@list.ru>
2019-05-29 22:11:59 +0300

    join point

--- algorithm-join-point/src/main/java/ru/vood/joinpoint/configuration/bpp/JoinPointBeanPostProcessor.kt
+++ algorithm-join-point/src/main/java/ru/vood/joinpoint/configuration/bpp/JoinPointBeanPostProcessor.kt
@@ -2,6 +2,7 @@ package ru.vood.joinpoint.configuration.bpp
 
 import org.springframework.beans.factory.config.BeanPostProcessor
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.query
 import org.springframework.stereotype.Component
 import ru.vood.joinpoint.configuration.infrastructure.jp.JoinPointService
 import sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl
@@ -18,8 +19,20 @@ class JoinPointBeanPostProcessor(private val jdbcTemplate: JdbcTemplate) : BeanP
             val find = bean.javaClass.genericInterfaces
                     .asSequence()
                     .find { type -> (type as ParameterizedTypeImpl).rawType == JoinPointService::class.java }
+            val parameterizedTypeImpl = find as ParameterizedTypeImpl
+            parameterizedTypeImpl.actualTypeArguments[1]
             println(find)
 
+            val methods = bean.javaClass.methods
+            println(methods)
+            val beanExists = jdbcTemplate.query("select DAB.BEAN_ID from DICT_ACT_BEAN DAB where DAB.BEAN_ID=:1",
+                    beanName) { rs, rowNum ->
+                rs.getString(1)
+            }
+
+
+            if (beanExists.isEmpty())
+                throw RuntimeException("Bean $beanName does not exists in table DICT_ACT_BEAN")
             val javaClass = bean.javaClass
             println(bean)
             println(beanName)
--- algorithm-join-point/src/main/resources/liquibase/release_001/task_001/package/test.sql
+++
@@ -1,8 +0,0 @@
-declare
-    i number;
-begin
-    i := RUN_FLOW.RUN('FLOW 2');
-    DBMS_OUTPUT.put_line(i);
-    commit;
-end;
-/
