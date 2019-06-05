709cf67e2e75a16ff8d1064057b74381c071c4ba
vood <balitsky_igor@list.ru>
2019-06-05 19:42:27 +0300

    join point начало изменения структуры

--- algorithm-join-point/src/main/resources/liquibase/release_001/task_001/view/100.act_order_join_point_vw.sql
+++ algorithm-join-point/src/main/resources/liquibase/release_001/task_001/view/100.act_order_join_point_vw.sql
@@ -1,54 +1,56 @@
 create or replace view act_order_join_point_vw as
-    with runner_only as (select runner.JOIN_POINT jp_runner,
-                                runner.FLOW       jp_runner_flow
-                         from DICT_ACT_RUNNER runner
-                                  join DICT_ACT_RUN run
-                                       on runner.JOIN_POINT = run.RUNNER and runner.FLOW = run.RUNNER_FLOW
-                         where not exists(select *
-                                          from DICT_ACT_RUN r
-                                          WHERE runner.JOIN_POINT = R.RUNNABLE
-                             --and runner.FLOW = r.RUNNABLE_FLOW
-                             )
-    ),
-         tree as (
-             select r.RUNNER,
-                    r.RUNNER_FLOW,
-                    r.IS_ASYNC_RUN,
-                    nvl(ro.jp_runner, r.RUNNABLE)                                                   RUNNABLE,
-                    --nvl(ro.jp_runner_flow, r.RUNNABLE_FLOW)                                         RUNNABLE_FLOW,
-                    --nvl(ro.jp_runner, r.RUNNABLE) || '~' || nvl(ro.jp_runner_flow, r.RUNNABLE_FLOW) id,
-                    nvl(ro.jp_runner, r.RUNNABLE) || '~' || ro.jp_runner_flow                       id,
-                    r.RUNNER || '~' || r.RUNNER_FLOW                                                parent
-             from DICT_ACT_RUN r
-                      full join runner_only ro on (ro.jp_runner_flow, ro.jp_runner) = ((ro.jp_runner_flow, r.RUNNABLE))
-         ),
-         ord as (
-             select level                                                         lv,
-                    CONNECT_BY_ISCYCLE                                            cycl,
-                    SYS_CONNECT_BY_PATH(t.RUNNABLE/*||'_'||RUNNABLE_FLOW*/, '->') path,
-                    t.RUNNER_FLOW                                                 FLOW,
-                    t.RUNNER,
-                    t.IS_ASYNC_RUN,
-                    t.RUNNABLE,
-                    id,
-                    parent
-             from tree t
-             start with t.parent = '~'
-             connect by NOCYCLE prior t.id = t.parent)
----
-    select o.*,
-           run_jp.BEAN_NAME      run_bean,
-           run_jp.RUN_CONTEXT    run_bean_in_ctx,
-           run_jp.RETURN_CONTEXT run_bean_ret_ctx,
-           run_jp.GLOBAL_TIMEOUT run_bean_timeout,
-           rbl_jp.BEAN_NAME      rbl_bean,
-           rbl_jp.RUN_CONTEXT    rbl_bean_in_ctx,
-           rbl_jp.RETURN_CONTEXT rbl_bean_ret_ctx,
-           rbl_jp.GLOBAL_TIMEOUT rbl_bean_timeout
+    with runner_only as (select runner.join_point jp_runner, runner.flow jp_runner_flow
+                         from dict_act_runner runner
+                                  join dict_act_run run
+                                       on runner.join_point = run.runner and runner.flow = run.runner_flow
+                         where not exists(
+                                 select * from dict_act_run r where runner.join_point = r.runnable))
+
+       ---
+       , tree as (select r.runner,
+                         r.runner_flow,
+                         r.is_async_run,
+                         nvl(ro.jp_runner, r.runnable)                                                 runnable,
+                         nvl(ro.jp_runner, r.runnable) || '~' || nvl(r.runner_flow, ro.jp_runner_flow) id,
+                         r.runner || '~' || r.runner_flow                                              parent
+                  from dict_act_run r
+                           full join runner_only ro
+                                     on (ro.jp_runner_flow, ro.jp_runner) = ((ro.jp_runner_flow, r.runnable)))
+       ---
+       , ord as (select level                                                         lv,
+                        connect_by_iscycle                                            cycl,
+                        SYS_CONNECT_BY_PATH(t.runnable/*||'_'||RUNNABLE_FLOW*/, '->') path,
+                        t.runner_flow                                                 flow,
+                        t.runner,
+                        t.is_async_run,
+                        t.runnable,
+                        id,
+                        parent
+                 from tree t
+                 start with t.parent = '~'
+                 connect by nocycle prior t.id = t.parent)
+       ---
+    select o.lv,
+           o.cycl,
+           o.path,
+           o.flow,
+           o.runner              runner_jp,
+           o.is_async_run,
+           o.runnable            runnable_jp,
+           o.id                  synthetic_id,
+           o.parent,
+           run_jp.bean_name      run_bean,
+           run_jp.run_context    run_bean_in_ctx_type,
+           run_jp.return_context run_bean_ret_ctx_type,
+           run_jp.global_timeout run_bean_timeout,
+           rbl_jp.bean_name      rbl_bean,
+           rbl_jp.run_context    rbl_bean_in_ctx_type,
+           rbl_jp.return_context rbl_bean_ret_ctx_type,
+           rbl_jp.global_timeout rbl_bean_timeout
     from ord o
-             left join DICT_ACT_JOIN_POINT rbl_jp on o.RUNNABLE = rbl_jp.id
-             left join DICT_ACT_JOIN_POINT run_jp on o.RUNNER = run_jp.id
-    order by o.flow, o.lv
+             left join dict_act_join_point rbl_jp on o.runnable = rbl_jp.id
+             left join dict_act_join_point run_jp on o.runner = run_jp.id
+    order by o.lv, o.flow nulls first
 --
 --
 -- /
