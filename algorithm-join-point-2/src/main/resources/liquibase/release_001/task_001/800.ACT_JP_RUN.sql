36a6a8fa3d967e3136e490c24e126d6d0c76aaf4
vood <balitsky_igor@list.ru>
2019-06-08 17:14:39 +0300

    join point -2

--- algorithm-join-point-2/src/main/resources/liquibase/release_001/task_001/800.ACT_JP_RUN.sql
+++ algorithm-join-point-2/src/main/resources/liquibase/release_001/task_001/800.ACT_JP_RUN.sql
@@ -2,20 +2,15 @@ create table act_jp_run
 (
     runner_id    varchar2(100) not null,
     runner_jp    varchar2(20)  not null,
+    constraint act_jp_run_runner_fk foreign key (runner_id, runner_jp) references jp.act_join_point (id, join_point),
     ---
     flow         varchar2(20)  not null,
     ---
     is_async_run number,
     constraint act_jp_run_async_ck check ( is_async_run in (0, 1)),
-
-    constraint act_jp_run_runner_fk foreign key (runner_id, runner_jp, flow, is_async_run)
-        references ACT_JP_RUNNER (id, JOIN_POINT, flow, is_async_run),
----
---    runnable_id  varchar2(100),
+    ---
     runnable_jp  varchar2(20),
-    --runnable_flow varchar2(20),
-    constraint act_jp_run_runnable_fk foreign key (runner_id, runnable_jp)
-        references ACT_JOIN_POINT (id, join_point),
+    constraint act_jp_run_runnable_fk foreign key (runner_id, runnable_jp) references jp.act_join_point (id, join_point),
     ---
 --     constraint act_jp_run_runnable_ck check ( (runnable_id is null and runnable_jp is null and runnable_flow is null) or
 --                                               (runnable_id is not null and runnable_jp is not null and
@@ -27,8 +22,6 @@ create table act_jp_run
 /
 create index act_jp_run_runner_i on act_jp_run (runner_id, runner_jp, flow) tablespace jp_idx
 /
---create index act_jp_run_runable_i on act_jp_run (runnable_id, runnable_jp, runnable_flow) tablespace jp_idx
---/
 comment on table act_jp_run
     is 'запускаемые бины '
 /
