661edf454ff1c194c11bb357d53509fa2166b700
vood <balitsky_igor@list.ru>
2019-06-09 12:46:16 +0300

    join point -2

--- algorithm-join-point-2/src/main/java/ru/vood/joinpoint2/infrastructure/flow/ActivityJoinPointOrderRunDao.kt
+++ algorithm-join-point-2/src/main/java/ru/vood/joinpoint2/infrastructure/flow/ActivityJoinPointOrderRunDao.kt
@@ -2,32 +2,48 @@ package ru.vood.joinpoint2.infrastructure.flow
 
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Service
-import ru.vood.joinpoint2.infrastructure.flow.data.DictionaryJoinPointData
-import ru.vood.joinpoint2.infrastructure.flow.mapper.DictionaryJoinPointDataRowMapper
+import ru.vood.joinpoint2.infrastructure.flow.data.JoinPointData
+import ru.vood.joinpoint2.infrastructure.flow.mapper.JoinPointDataRowMapper
 
 @Service
 class ActivityJoinPointOrderRunDao(private val jdbcTemplate: JdbcTemplate) : ActivityJoinPointOrderRunDaoService {
 
-    val rowMapperDictionaryJoinPointData = DictionaryJoinPointDataRowMapper()
+    val rowMapperJoinPointData = JoinPointDataRowMapper()
 
-    override fun nextJoinPoints(id: Int, joinPoint: String, flowType: String): Map<String, DictionaryJoinPointData> {
+    override fun nextJoinPoints(id: Int, joinPoint: String, flowType: String): Map<String, JoinPointData> {
         return jdbcTemplate.query(
-                """select lv, cycl, flow, runner_jp, is_async_run, runnable_jp, synthetic_id, parent, run_bean, run_bean_in_ctx_type, run_bean_ret_ctx_type, run_bean_timeout, rbl_bean, rbl_bean_in_ctx_type, rbl_bean_ret_ctx_type, rbl_bean_timeout
-                            from dict_act_ordered_jp_vw jp
-                            where jp.flow=:1 and runner_jp=:2""",
-                rowMapperDictionaryJoinPointData,
-                flowType, joinPoint
+                """select lv, cycl, flow, runner_jp, is_async_run, runnable_jp, synthetic_id, parent, run_bean, run_bean_in_ctx_type, run_bean_ret_ctx_type, run_bean_timeout, rbl_bean, rbl_bean_in_ctx_type, rbl_bean_ret_ctx_type, rbl_bean_timeout, runner_id
+                            from act_ordered_jp_vw jp
+                            where jp.flow=:1 and jp.runner_jp=:2 and jp.runner_id=:3""",
+                rowMapperJoinPointData,
+                flowType, joinPoint, id
         ).asSequence()
-                .map { Pair<String, DictionaryJoinPointData>(it.runnable, it) }
+                .map { Pair<String, JoinPointData>(it.runnable, it) }
                 .toMap()
     }
 
-    override fun prevJoinPoints(id: Int, joinPoint: String, flowType: String): Map<String, DictionaryJoinPointData> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun prevJoinPoints(id: Int, joinPoint: String, flowType: String): Map<String, JoinPointData> {
+        return jdbcTemplate.query(
+                """select lv, cycl, flow, runner_jp, is_async_run, runnable_jp, synthetic_id, parent, run_bean, run_bean_in_ctx_type, run_bean_ret_ctx_type, run_bean_timeout, rbl_bean, rbl_bean_in_ctx_type, rbl_bean_ret_ctx_type, rbl_bean_timeout, runner_id
+                            from act_ordered_jp_vw jp
+                            where jp.flow=:1 and jp.runner_jp is not null and jp.runnable_jp=:2 and jp.runner_id:3""",
+                rowMapperJoinPointData,
+                flowType, joinPoint, id
+        ).asSequence()
+                .map { Pair<String, JoinPointData>(it.runner.toString(), it) }
+                .toMap()
     }
 
-    override fun getFirstJoinPoint(id: Int, flowType: String): Map<String, DictionaryJoinPointData> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun getFirstJoinPoint(id: Int, flowType: String): Map<String, JoinPointData> {
+        return jdbcTemplate.query(
+                """select lv, cycl, flow, runner_jp, is_async_run, runnable_jp, synthetic_id, parent, run_bean, run_bean_in_ctx_type, run_bean_ret_ctx_type, run_bean_timeout, rbl_bean, rbl_bean_in_ctx_type, rbl_bean_ret_ctx_type, rbl_bean_timeout, runner_id
+                            from act_ordered_jp_vw jp
+                            where jp.flow=:1 and jp.parent='~' and jp.runner_id=:2""",
+                rowMapperJoinPointData,
+                flowType, id
+        ).asSequence()
+                .map { Pair<String, JoinPointData>(it.runner.toString(), it) }
+                .toMap()
     }
 
 
--- algorithm-join-point-2/src/main/java/ru/vood/joinpoint2/infrastructure/flow/ActivityJoinPointOrderRunDaoService.kt
+++ algorithm-join-point-2/src/main/java/ru/vood/joinpoint2/infrastructure/flow/ActivityJoinPointOrderRunDaoService.kt
@@ -1,12 +1,12 @@
 package ru.vood.joinpoint2.infrastructure.flow
 
-import ru.vood.joinpoint2.infrastructure.flow.data.DictionaryJoinPointData
+import ru.vood.joinpoint2.infrastructure.flow.data.JoinPointData
 
 interface ActivityJoinPointOrderRunDaoService {
 
-    fun nextJoinPoints(id: Int, joinPoint: String, flowType: String): Map<String, DictionaryJoinPointData>
+    fun nextJoinPoints(id: Int, joinPoint: String, flowType: String): Map<String, JoinPointData>
 
-    fun prevJoinPoints(id: Int, joinPoint: String, flowType: String): Map<String, DictionaryJoinPointData>
+    fun prevJoinPoints(id: Int, joinPoint: String, flowType: String): Map<String, JoinPointData>
 
-    fun getFirstJoinPoint(id: Int, flowType: String): Map<String, DictionaryJoinPointData>
+    fun getFirstJoinPoint(id: Int, flowType: String): Map<String, JoinPointData>
 }
