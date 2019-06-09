917aa5a7681ee523f7360347532942c0511b807d
vood <balitsky_igor@list.ru>
2019-06-09 12:34:26 +0300

    join point -2

--- algorithm-join-point-2/src/test/java/ru/vood/joinpoint2/infrastructure/flow/DictionaryJoinPointOrderRunDaoTest.kt
+++ algorithm-join-point-2/src/test/java/ru/vood/joinpoint2/infrastructure/flow/DictionaryJoinPointOrderRunDaoTest.kt
@@ -7,7 +7,7 @@ import ru.vood.joinpoint2.config.AbstractJoinPointDataSourceTest
 class DictionaryJoinPointOrderRunDaoTest : AbstractJoinPointDataSourceTest() {
 
     lateinit var dictionaryJoinPointOrderRunDao: DictionaryJoinPointOrderRunDao
-    val flowType_3 = FlowType.FLOW_3.flowName
+    val FLOW_TYPE_3 = FlowType.FLOW_3.flowName
 
     override fun postConstructLocal() {
         super.postConstructLocal()
@@ -16,7 +16,7 @@ class DictionaryJoinPointOrderRunDaoTest : AbstractJoinPointDataSourceTest() {
 
     @Test
     fun nextJoinPoints() {
-        val prevJoinPoints = dictionaryJoinPointOrderRunDao.nextJoinPoints("join point 4", flowType_3)
+        val prevJoinPoints = dictionaryJoinPointOrderRunDao.nextJoinPoints("join point 4", FLOW_TYPE_3)
         Assert.assertEquals(2, prevJoinPoints.size)
         Assert.assertTrue(prevJoinPoints.keys.contains("join point 5"))
         Assert.assertTrue(prevJoinPoints.keys.contains("join point 6"))
@@ -24,7 +24,7 @@ class DictionaryJoinPointOrderRunDaoTest : AbstractJoinPointDataSourceTest() {
 
     @Test
     fun prevJoinPoints() {
-        val prevJoinPoints = dictionaryJoinPointOrderRunDao.prevJoinPoints("join point 4", flowType_3)
+        val prevJoinPoints = dictionaryJoinPointOrderRunDao.prevJoinPoints("join point 4", FLOW_TYPE_3)
         Assert.assertEquals(2, prevJoinPoints.size)
         Assert.assertTrue(prevJoinPoints.keys.contains("join point 3"))
         Assert.assertTrue(prevJoinPoints.keys.contains("join point 2"))
@@ -36,7 +36,7 @@ class DictionaryJoinPointOrderRunDaoTest : AbstractJoinPointDataSourceTest() {
 
     @Test
     fun getFirstJoinPoint() {
-        val firstJoinPoint = dictionaryJoinPointOrderRunDao.getFirstJoinPoint(flowType_3)
+        val firstJoinPoint = dictionaryJoinPointOrderRunDao.getFirstJoinPoint(FLOW_TYPE_3)
         Assert.assertEquals(1, firstJoinPoint.size)
         Assert.assertEquals("join point 1", firstJoinPoint.keys.first())
     }
