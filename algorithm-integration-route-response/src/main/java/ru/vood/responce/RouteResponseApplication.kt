ad407642235f379e42640c6a0e2b2ebe6d5d7b14
vood <balitsky_igor@list.ru>
2019-06-15 19:52:03 +0300

    route

--- algorithm-integration-route-response/src/main/java/ru/vood/responce/RouteResponseApplication.kt
+++ algorithm-integration-route-response/src/main/java/ru/vood/responce/RouteResponseApplication.kt
@@ -1,17 +1,19 @@
 package ru.vood.responce
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @SpringBootApplication
 @EnableTransactionManagement
-
 open class RouteResponseApplication {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            SpringApplication.run(RouteResponseApplication::class.java, *args)
-        }
-    }
+//    companion object {
+//        @JvmStatic
+//        fun main(args: Array<String>) {
+//            SpringApplication.run(RouteResponseApplication::class.java, *args)
+//        }
+//    }
+}
+
+fun main(args: Array<String>) {
+    SpringApplication.run(RouteResponseApplication::class.java, *args)
 }
