39b73c238bf5d204eaa34762422c6171f5072fb1
vood <balitsky_igor@list.ru>
2019-12-22 13:12:09 +0300

    yaml

---
+++ yaml-reader/src/test/java/ru/vood/yaml/Contact.java
@@ -0,0 +1,22 @@
+package ru.vood.yaml;
+
+public class Contact {
+    private String type;
+    private int number;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
